6c2d1dfe70c8257f9daa1a99205ce1210861dfe2
יובל אברמוב <yuvalhd711@gmail.com>
2026-01-26 12:37:21 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,13 +1,22 @@
+import Admin from './pages/Admin';
+import Branches from './pages/Branches';
+import Console from './pages/Console';
+import Display from './pages/Display';
 import Home from './pages/Home';
-import __Layout from './Layout.jsx';
-
+import Kiosk from './pages/Kiosk';
+import __Layout from './Layout.js';
 
 export const PAGES = {
+    "Admin": Admin,
+    "Branches": Branches,
+    "Console": Console,
+    "Display": Display,
     "Home": Home,
+    "Kiosk": Kiosk,
 }
 
 export const pagesConfig = {
-    mainPage: "Home",
+    mainPage: "Kiosk",
     Pages: PAGES,
     Layout: __Layout,
 };
